f531ef097826c830d86286df0211b4434530b790
golukr9876 <golukumar02052007@gmail.com>
2025-03-26 19:10:52 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -19,8 +19,8 @@ const registerUser = asyncHandler( async (req, res) => {
 
     //get user details
     const {fullName, email, username, password} = req.body
-    console.log("emil: ", email);
-    console.log("password: ", password);
+    // console.log("emil: ", email);
+    // console.log("password: ", password);
 
 
     //validation-not empty
@@ -32,7 +32,7 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     //check if user is already exist
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{username}, {email}]
     })
 
@@ -40,9 +40,16 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(409, "User with email or username already exists")
     }
 
+    // console.log(req.files);
+
     //images, avatar
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,7 +1,11 @@
 import mongoose, {Schema} from "mongoose";
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
-import { JsonWebTokenError } from "jsonwebtoken";
+// import { JsonWebTokenError } from "jsonwebtoken";
+import pkg from 'jsonwebtoken';
+const { JsonWebTokenError } = pkg;
+
+
 
 const userSchema = new Schema(
     {
